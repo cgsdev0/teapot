325c14142fe3364e534bdfc2f3cd877589428fd5
Sarah Schulte <git@sarah.engineer>
2026-06-30 12:01:42 -0400

    data get

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ wabt cargo rustc llvmPackages.bintools ];
+        packages = with pkgs; [ rustfmt wabt cargo rustc llvmPackages.bintools ];
       };
     };
 }
