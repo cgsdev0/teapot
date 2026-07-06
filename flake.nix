f4d1485e2a54bb01d5b129368b9f908422d4ec25
Sarah Schulte <git@sarah.engineer>
2026-07-06 00:10:59 -0400

    fix the flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,34 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays = [
-        ];
+        overlays = [ (import rust-overlay) ];
       };
 
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "wasm32-unknown-unknown" ];
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [clippy rust-analyzer rustfmt wabt cargo rustc llvmPackages.bintools ];
+        packages = with pkgs; [
+          rustToolchain  
+          wabt
+          llvmPackages.bintools
+        ];
+
+        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
       };
     };
 }
+
