24ccf160cf349fc9a737f34f73ff637094dc3d60
Sarah Schulte <git@sarah.engineer>
2026-07-08 17:39:35 -0400

    we hello worldin

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use raylib::prelude::*;
 
+mod app;
+mod geometry;
+
+use crate::app::*;
+use crate::geometry::*;
+
 fn main() {
     let (mut rl, thread) = raylib::init().size(1030, 765).title("Hello, World").build();
+    let app = AppState::new();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        d.clear_background(Color::BLACK);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::WHITE);
     }
 }
